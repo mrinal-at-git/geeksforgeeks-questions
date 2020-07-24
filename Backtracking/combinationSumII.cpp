ffdbcfeeb7cdd9a9cc8d6f59a3add4b3361a88d6
mrinal-at-git <60399993+mrinal-at-git@users.noreply.github.com>
2020-07-24 20:28:39 +0530

    Create combinationSumII.cpp

---
+++ Backtracking/combinationSumII.cpp
@@ -0,0 +1,99 @@
+Question Link - https://practice.geeksforgeeks.org/problems/combination-sum-part-2/0
+
+
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void findCombinations(vector <int> &a, int sum, vector <int> &r, vector<vector<int> >&res, int i){
+    
+if(sum <0) return;
+
+if(sum ==0) {
+       res.push_back(r);
+       return;      
+}
+
+
+
+while(i < a.size() && sum- a[i] >=0){
+    
+    r.push_back(a[i]);
+    i++;
+    findCombinations(a, sum-a[i-1], r, res, i);
+    
+    while(i < a.size() && a[i] == a[i - 1]) {
+        i++;
+    }
+    r.pop_back();
+    
+  }
+
+}
+
+
+
+vector<vector<int> >combinationSum(vector<int>& a, int sum){
+    
+    sort(a.begin(), a.end());
+
+    
+    vector<int> r;
+    vector<vector<int> > res;
+    
+    findCombinations(a, sum, r, res, 0);
+    
+    return res;
+}
+
+
+
+int main() {
+    int t;
+    cin >>t;
+    while(t--){
+        
+      int n, temp;
+      cin >>n;
+      vector<int>a;
+    
+      for(int i=0;i<n;i++){
+          cin >> temp;
+          a.push_back(temp);
+      }
+        
+       
+      int sum;
+      cin >> sum;
+      
+      vector<vector<int> > result = combinationSum(a, sum);
+        
+      if(result.size() == 0) cout << "Empty";  
+       
+       else{
+           
+           for(int i =0;i<result.size();i++){
+               
+               
+            if (result[i].size() > 0) {
+              cout<< "("; 
+              
+              for(int j = 0;j<result[i].size(); j++){
+                  
+                  if(j == result[i].size()-1) cout << result[i][j];
+                  else cout << result[i][j] << " ";
+              }
+              cout<< ")"; 
+               
+             }
+           }
+    
+       } 
+    
+     cout << endl;
+        
+    }
+	return 0;
+}
+
